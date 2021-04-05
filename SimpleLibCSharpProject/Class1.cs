52db91a3dc51643df19719d0a56ad3adabe3fbea
michael-mandang-pdftron <75691948+michael-mandang-pdftron@users.noreply.github.com>
2021-04-05 04:35:23 -0700

    Add files via upload

---
+++ SimpleLibCSharpProject/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleLibCSharpProject
+{
+    public class Class1
+    {
+
+        public string function1()
+        {
+            return "function1";
+        }
+
+        public string function2(string msg)
+        {
+            return msg;
+        }
+    }
+}
